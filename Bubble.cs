31c91313cb13c51a43b0a6eea7a95c5eedb5f41b
Yassen Ismail <82486618+Yassenismail12@users.noreply.github.com>
2023-05-22 00:25:55 +0300

    Create Bubble.cs

---
+++ Bubble.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 4, 6, 1, 3 };
+
+        Console.WriteLine("Original Array:");
+        PrintArray(arr);
+
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("\nSorted Array:");
+        PrintArray(arr);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
